36138974aed1608699152bfda49e7ae3a5898082
Nematullo Nurmamatov <nematullohataboev@gmail.com>
2024-11-06 20:41:01 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,95 @@
+// masala 1
+
+
+// let M = +prompt('Oyni kiriting')
+// let D = +prompt('Sanasini kiriting')
+
+// function getDate(M, D) {
+//     const hisoblash = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+
+//     if (M < 1 || M > 12 || D < 1 || D > hisoblash[M - 1]) {
+//         return "Noto'g'ri sanalar.";
+//     }
+
+//     return `Berilgan sana: ${M} oy ${D} - kuni  `;
+// }
+
+// console.log(getDate(M, D));
+
+
+
+
+// masala 2
+
+
+// function san() {
+//     users.forEach((user) => {
+//         let antonio = user.address.city == 'san Antonio';
+
+//         if (antonio) {
+//             console.log("usernamesi:", user.username,"   ", "email: ", user.email);
+//         }
+
+//     })
+// }
+
+// san()
+
+
+
+
+// masala 3
+
+
+// function manzil() {
+//     let addres = users.map((user) => {
+//         return {
+//             street: user.address.street,
+//             city: user.address.city,
+//             zip: user.address.zipcode
+//         }
+//     })
+    
+//     console.log(addres);
+// }
+
+// manzil()
+
+
+
+
+
+
+// masala 4
+
+// users.sort((a, b) => a.username.localeCompare(b.username));
+
+// console.log(users);
+
+
+
+// masala 5
+
+// let number = prompt("Son kiriting:");
+
+// function reverseNumber(num) {
+//     let reversed = parseInt(num.toString().split('').reverse().join(''), 10);
+//     console.log("Teskari son:", reversed);
+// }
+
+// reverseNumber(number);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
